864d6baedb01b806b8a3aa5bd5996af77a415203
Benoit Moussaud <benoit@moussaud.org>
2023-04-18 10:23:16 +0200

    make apply workload aware of the gateway

--- apply_workload.sh
+++ apply_workload.sh
@@ -1,3 +1,5 @@
 NS=micropets-dev
+#PETS_SERVICE="http://micropets-gateway.tap5.eu.aks.mytanzu.xyz/dev/pets"
+PETS_SERVICE="http://pets-golang.micropets-dev/pets"
 kubectl apply -f config/gui_config.yaml --namespace $NS
-tanzu apps workload apply -f config/workload.yaml -e "PETS_SERVICE=http://pets-golang.micropets-dev/pets" --live-update --local-path . --source-image akseutap5registry.azurecr.io/gui  --yes  --update-strategy merge --namespace $NS
+tanzu apps workload apply -f config/workload.yaml -e "PETS_SERVICE=${PETS_SERVICE}" --live-update --local-path . --source-image akseutap5registry.azurecr.io/gui  --yes  --update-strategy merge --namespace $NS
