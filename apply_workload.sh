2a957e1cbbc1ddfb2e2579e4c1c49eea2d27c19f
Benoit Moussaud <benoit@moussaud.org>
2023-07-26 15:17:08 +0200

    workload with test

--- apply_workload.sh
+++ apply_workload.sh
@@ -3,5 +3,5 @@ NS=micropets-dev
 PETS_SERVICE="http://pets-golang.micropets-dev/pets" 
 #without Spring Cloud Gateway
 #PETS_SERVICE="https://micropets-gateway.micropets-dev/api/public/pets"
-kubectl apply -f config/gui_config.yaml --namespace $NS
-tanzu apps workload apply -f config/workload.yaml -e "PETS_SERVICE=${PETS_SERVICE}" --live-update --local-path . --source-image akseutap5registry.azurecr.io/gui  --yes  --update-strategy merge --namespace $NS
+#kubectl apply -f config/gui_config.yaml --namespace $NS
+tanzu apps workload apply -f config/workload.yaml -e "PETS_SERVICE=${PETS_SERVICE}" --live-update --local-path . --source-image akseutap6registry.azurecr.io/gui  --yes  --update-strategy merge --namespace $NS
