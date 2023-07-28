ad5936d24ac3a8da62e7674282fc0285a120425a
Benoit Moussaud <benoit@moussaud.org>
2023-07-28 14:46:57 +0200

    use lsp

--- apply_workload.sh
+++ apply_workload.sh
@@ -4,4 +4,4 @@ PETS_SERVICE="http://pets-golang.micropets-dev/pets"
 #without Spring Cloud Gateway
 #PETS_SERVICE="https://micropets-gateway.micropets-dev/api/public/pets"
 #kubectl apply -f config/gui_config.yaml --namespace $NS
-tanzu apps workload apply -f config/workload.yaml -e "PETS_SERVICE=${PETS_SERVICE}" --live-update --local-path . --source-image akseutap6registry.azurecr.io/gui  --yes  --update-strategy merge --namespace $NS
+tanzu apps workload apply -f config/workload.yaml -e "PETS_SERVICE=${PETS_SERVICE}" --live-update --local-path .  --yes  --update-strategy merge --namespace $NS
