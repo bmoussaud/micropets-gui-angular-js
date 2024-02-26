fef11ae21da3dc2e3ede4e936e4fea3477c3c782
Benoit Moussaud <benoit@moussaud.org>
2024-02-26 10:14:17 +0100

    GATEWAY_SERVICE

--- apply_workload.sh
+++ apply_workload.sh
@@ -1,4 +1,1 @@
-set -x
-NS=micropets-dev
-PETS_SERVICE="https://micropets-gateway.micropets-dev/micropets/pets"
-tanzu apps workload apply --file config/workload.yaml -e "PETS_SERVICE=${PETS_SERVICE}" --namespace ${NS} --debug --yes --local-path . --live-update --tail --update-strategy replace
+tanzu apps workload apply --file config/workload.yaml --namespace micropets-dev --debug --yes --local-path . --live-update --tail --update-strategy replace
